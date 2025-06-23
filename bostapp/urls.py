6470b6789a52da8c032531097124c627cc141795
Awuah-B <baffourjr99@gmail.com>
2025-06-23 13:24:36 +0000

    config

--- bostapp/urls.py
+++ bostapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import home, export_csv, preview_pdf, download_pdf
 
+app_name = 'bostapp'  # This is crucial for namespace registration
+
 urlpatterns = [
     path('', home, name='home'),
     path('export-csv/', export_csv, name='export_csv'),
